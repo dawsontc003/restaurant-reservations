f507b626c6aa0ca188e440dbc7e6cfb999e9509b
Shawn McPhee <scmcphee9@gmail.com>
2020-12-05 10:57:26 -0700

    created routes for the different sites that will be required

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const express = require("express");
 const app = express();
 const PORT = 9000;
 
-const table = {
-  name: "Joe",
-  phoneNumber: "123456789",
-  customerNumber: 123,
-  customerId: 2,
-};
+const tables = [
+  { name: "Joe", phoneNumber: "123456789", customerNumber: 123, customerId: 2 },
+];
+
+const waitList = [];
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Hot Restaurant");
+});
+
+app.get("/makereservation", (req, res) => {
+  res.send("Make Reservation");
+});
+
+app.get("/viewtables", (req, res) => {
+  res.send(tables);
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
